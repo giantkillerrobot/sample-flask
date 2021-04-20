d1a83d3bd614c320f82b051205769fc16357f15e
Dion McCormick <dion@giantkillerrobot.com>
2021-04-19 20:59:47 -0500

    Added logging and print statement

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask import render_template
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
+    print('hello')
+    logging.debug('hello')
     return render_template("index.html")
